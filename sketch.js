d45d21a383edfd1d165d7670ba7e7ccc5927a36e
pedrou2000 <urbina2000pedro@gmail.com>
2023-11-06 09:52:09 +0000

    updated ui and radial

--- sketch.js
+++ sketch.js
@@ -5,39 +5,70 @@ const SYMMETRY_OPTIONS = {
     EIGHT_WAY: 8,
     TEN_WAY: 10,
     TWELVE_WAY: 12,
+    RADIAL: 'radial', // Radial symmetry option
 };
 
 let symmetry = SYMMETRY_OPTIONS.FOUR_WAY; // Default symmetry
+let petals = 10; // Default number of petals
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(255);
     setupSymmetrySelector();
+    // Add event listener for the petal slider
+    const petalSlider = document.getElementById('petalSlider');
+    petalSlider.addEventListener('input', function(event) {
+        petals = event.target.value;
+    });
+
+    // Add event listener for the clear button
+    const clearButton = document.getElementById('clearButton');
+    clearButton.addEventListener('click', function() {
+        clearCanvas();
+    });
+}
+// Function to clear the canvas
+function clearCanvas() {
+    clear(); // Clears the pixels within a buffer
+    background(255); // Set to white background
 }
 
 function setupSymmetrySelector() {
     const selector = document.getElementById('symmetrySelector');
     selector.addEventListener('change', function(event) {
-        setSymmetry(int(event.target.value));
+        setSymmetry(event.target.value);
     });
 }
 
 function setSymmetry(newSymmetry) {
     symmetry = newSymmetry;
-    background(255); // Clear the canvas
 }
 
 function drawSymmetricalLines(x, y, px, py) {
-    const angle = TWO_PI / symmetry;
-    for (let i = 0; i < symmetry; i++) {
-        // Main line
-        drawLineAtAngle(x, y, px, py, angle * i);
-        // Reflected line
-        drawLineAtAngle(x, y, px, py, angle * i, true);
+    if (symmetry === SYMMETRY_OPTIONS.RADIAL) {
+        drawRadialLines(x, y, px, py);
+    } else {
+        const angle = TWO_PI / symmetry;
+        for (let i = 0; i < symmetry; i++) {
+            drawLineAtAngle(x, y, px, py, angle * i);
+            drawLineAtAngle(x, y, px, py, angle * i, true);
+        }
     }
 }
 
-function drawLineAtAngle(x, y, px, py, angle, reflect=false) {
+function drawRadialLines(x, y, px, py) {
+    const angle = TWO_PI / petals;
+    for (let i = 0; i < petals; i++) {
+        push();
+        translate(width / 2, height / 2);
+        rotate(angle * i);
+        let sx = 0; // Start at the center for radial symmetry
+        let sy = 0;
+        line(sx, sy, x, y);
+        pop();
+    }
+}
+
+function drawLineAtAngle(x, y, px, py, angle, reflect = false) {
     push();
     translate(width / 2, height / 2);
     rotate(angle);
@@ -47,7 +78,7 @@ function drawLineAtAngle(x, y, px, py, angle, reflect=false) {
 }
 
 function draw() {
-    if (mouseIsPressed) {
+    if (mouseIsPressed && mouseOverCanvas()) {
         const mx = mouseX - width / 2;
         const my = mouseY - height / 2;
         const pmx = pmouseX - width / 2;
@@ -55,8 +86,11 @@ function draw() {
         drawSymmetricalLines(mx, my, pmx, pmy);
     }
 }
+// Helper function to check if the mouse is over the canvas
+function mouseOverCanvas() {
+    return mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height;
+}
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-    background(255);
 }
