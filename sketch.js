b50050e3663b0103c8c9ee7a5d431af48ac5f357
pedrou2000 <urbina2000pedro@gmail.com>
2023-11-06 10:44:28 +0000

    a

--- sketch.js
+++ sketch.js
@@ -1,35 +1,38 @@
-// Define the mode of drawing and the default values for symmetries and petals
 const Mode = {
     SYMMETRY: 'symmetry',
     RADIAL: 'radial'
 };
 
-// Starting defaults
 let currentMode = Mode.SYMMETRY;
-let symmetry = 4; // Default to four-way symmetry as a sensible default
+let symmetry = 4; // Default to four-way symmetry
 let petals = 10; // Default number of petals
 
-// Set up the canvas and event listeners when the document is loaded
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', setupUI);
+
+function setupUI() {
     const symmetriesButton = document.getElementById('symmetriesButton');
     const radialButton = document.getElementById('radialButton');
     const petalSlider = document.getElementById('petalSlider');
     const symmetrySelector = document.getElementById('symmetrySelector');
+    const clearButton = document.getElementById('clearButton');
 
     symmetriesButton.addEventListener('click', () => toggleMode(Mode.SYMMETRY));
     radialButton.addEventListener('click', () => toggleMode(Mode.RADIAL));
     petalSlider.addEventListener('input', (e) => petals = e.target.value);
     symmetrySelector.addEventListener('change', (e) => symmetry = e.target.value);
-});
+    clearButton.addEventListener('click', clearCanvas);
+}
 
-// Toggle between symmetry and radial modes
 function toggleMode(mode) {
     currentMode = mode;
+    updateUIForMode(mode);
+}
+
+function updateUIForMode(mode) {
     document.getElementById('symmetryOptions').classList.toggle('collapse', mode !== Mode.SYMMETRY);
     document.getElementById('radialOptions').classList.toggle('collapse', mode !== Mode.RADIAL);
 }
 
-// p5.js functions for drawing
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255);
@@ -41,18 +44,15 @@ function draw() {
     }
 }
 
-// Simplified function to draw lines according to the current mode
 function drawSymmetricalLines(x, y, px, py) {
     const count = currentMode === Mode.SYMMETRY ? symmetry : petals;
     const angle = TWO_PI / count;
 
     for (let i = 0; i < count; i++) {
-        const currentAngle = angle * i;
-        drawLineAtAngle(x, y, px, py, currentAngle, currentMode === Mode.SYMMETRY);
+        drawLineAtAngle(x, y, px, py, angle * i, currentMode === Mode.SYMMETRY);
     }
 }
 
-// Drawing a line at a given angle and optionally reflecting it
 function drawLineAtAngle(x, y, px, py, angle, reflect = false) {
     push();
     translate(width / 2, height / 2);
@@ -62,13 +62,15 @@ function drawLineAtAngle(x, y, px, py, angle, reflect = false) {
     pop();
 }
 
-// Check if the mouse is over the canvas area
 function mouseOverCanvas() {
     return mouseX >= 0 && mouseX < width && mouseY >= 0 && mouseY < height;
 }
 
-// Adjust canvas size when the window is resized
+function clearCanvas() {
+    background(255);
+}
+
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-    background(255);
+    clearCanvas(); // Clear the canvas to white after resizing
 }
